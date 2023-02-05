27d0737551e51a79f19e7dc2fd38772e8b8f00b6
Stephen Walton <info@stevethedeveloper.com>
2023-02-05 03:11:34 +0000

    testing git

--- backend/index.php
+++ backend/index.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__ . "/config/bootstrap.php";
-$uri = parse_url($_SERVER['PATH_INFO'], PHP_URL_PATH);
+$url = parse_url($_SERVER['PATH_INFO'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
 if ((isset($uri[1]) && $uri[1] != 'url') || !isset($uri[2])) {
     header(RESPONSE_HEADERS['404']);
